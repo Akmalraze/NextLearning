d449d84db57005c33409556777a4300b587e32c4
Akmalraze <165366805+Akmalraze@users.noreply.github.com>
2025-12-29 09:48:46 +0800

    .

--- app/Models/Assessments.php
+++ app/Models/Assessments.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\AssessmentQuestion;
+use App\Models\AssessmentMaterial;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
